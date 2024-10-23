1c33974065273a9f45e5996fcb2b0505b10e6482
Kamalesh Parmar <kamalesh.py@gmail.com>
2024-10-23 13:16:15 +0530

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-# Python script to add two integers, taking input from the user.
-
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
-
-result = x+y
-print(f"The sum total of {x} and {y} is {result}")
-# input()
